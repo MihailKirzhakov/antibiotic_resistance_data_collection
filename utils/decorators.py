95eae2a46db8a52c56ff7598fb345f676f25fb75
Mihail Kirzhakov <kirzhakovma@mail.ru>
2025-04-25 14:32:24 +0300

    'Установлена перезапись лог файла, если размер превышает 1кб'

--- utils/decorators.py
+++ utils/decorators.py
@@ -6,7 +6,14 @@ from contextlib import contextmanager
 @contextmanager
 def redirect_stderr_to_log():
     """Контекстный менеджер для перенаправления stderr в файл логов."""
-    with open(os.path.join(os.getcwd(), 'log_info.txt'), 'w') as log_file:
+    log_file_path = os.path.join(os.getcwd(), 'log_info.txt')
+
+    # Проверка размера файла и перезапись, если он превышает 1кб
+    if os.path.exists(log_file_path) and os.path.getsize(log_file_path) > 8000:
+        with open(log_file_path, 'w') as log_file:
+            pass
+
+    with open(log_file_path, 'a') as log_file:
         old_stderr = sys.stderr
         sys.stderr = log_file
         try:
