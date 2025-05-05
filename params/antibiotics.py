b0e307df0864a2501a95d2553601bc4d99eb5e52
Mihail Kirzhakov <kirzhakovma@mail.ru>
2025-05-05 16:53:47 +0300

    'Сокращен список антибиотиков для проверки'

--- params/antibiotics.py
+++ params/antibiotics.py
@@ -1,28 +1,37 @@
 # Список антибиотиков для проверки на резистентность
 antibiotic_list = [
-    'Penicillin', 'Oxacillin', 'Ampicillin', 'Amoxicillin',
-    'Amoxicillin/Clavulanic', 'Ampicillin/Sulbactam',
-    'Azlocillin', 'Carbenicillin', 'Carbenicillin-Ps', 'Ticarcillin',
-    'Ticarcillin/Clavulanic', 'Mezlocillin', 'Piperacillin',
+    'Penicillin', 'Ampicillin', 'Amoxicillin',
+    'Amoxicillin/Clavulanic', 'Ticarcillin',
+    'Ticarcillin/Clavulanic', 'Piperacillin',
     'Piperacillin/Tazobactam', 'Imipenem', 'Meropenem',
-    'Ertapenem', 'Aztreonam', 'ESβL Cft/CA', 'ESβL  Caz/CA',
-    'Cephalothin', 'Cefazolin', 'Cefuroxime', 'Cefotetan',
-    'Cefamandole', 'Cefoxitin', 'Cefonicid', 'Cefaclor',
-    'Loracarbef', 'Cefotaxime', 'Ceftriaxone', 'Ceftizoxime',
-    'Cefixime', 'Cefpodoxime', 'Cefoperazone', 'Ceftazidime',
-    'Cefepime', 'Gentamicin', 'Gentamicin syn', 'Streptomyc.syn',
-    'Amikacin', 'Tobramycin', 'Netilmicin', 'Spectinomycin',
-    'Ciprofloxacin', 'Gatifloxacin', 'Moxifloxacin', 'Ofloxacin',
-    'Lomefloxacin', 'Levofloxacin', 'Pefloxacin', 'Sparfloxacin',
+    'Aztreonam', 'Cefazolin', 'Cefuroxime', 'Cefoxitin',
+    'Cefotaxime', 'Ceftriaxone', 'Cefixime', 'Ceftazidime',
+    'Cefepime', 'Gentamicin', 'Amikacin', 'Tobramycin',
+    'Ciprofloxacin', 'Moxifloxacin', 'Levofloxacin',
     'Erytromycin', 'Clarithromycin', 'Azithromycin', 'Clindamycin',
-    'Fosfomycin', 'Josamycin', 'Tetracycline', 'Minocycline',
+    'Fosfomycin', 'Tetracycline', 'Minocycline',
     'Doxiciclin', 'Trimethoprim-Sulphamethoxazole',
     'Chloramphenicol', 'Rifampicin',
-    'Vancomycin', 'Linezolid', 'Synercid', 'Tigecycline',
-    'Daptomicin', 'Metronidazol', 'Norfloxacin', 'Cinoxacin',
-    'Nalidixic Acid', 'Nitrofurantoin', 'Sulfamethoxazole', 'Trimethoprim',
-    '5-НОК', 'Voriconazol', 'Amphotericin B', 'Miconazol',
+    'Vancomycin', 'Linezolid', 'Tigecycline',
+    'Daptomicin', 'Norfloxacin', 'Nitrofurantoin',
+    'Voriconazol', 'Amphotericin B', 'Miconazol',
     'Ketoconazole', 'Itraconazole', 'Fluconazole'
+]
+
+CR_ESBLS_CULTURES = [
+    'Achromobacter xylosoxidans', 'Achromobacter xylosoxidans subsp denitrificans',
+    'Acinetobacter baumannii', 'Acinetobacter pittii', 'Acinetobacter radioresistens',
+    'Alcaligenes faecalis', 'Burkholderia cepacia', 'Citrobacter braakii',
+    'Citrobacter freundii', 'Citrobacter koseri', 'Citrobacter koseri (diversus)',
+    'Enterobacter asburiae', 'Enterobacter cloacae', 'Enterobacter hormaechei',
+    'Enterobacter kobei', 'Enterobacter ludwigii', 'Escherichia coli',
+    'Klebsiella aerogenes', 'Klebsiella pneumoniae', 'Klebsiella oxytoca',
+    'Klebsiella variicola', 'Morganella morganii', 'Pasteurella multocida',
+    'Proteus hauseri', 'Proteus mirabilis', 'Proteus vulgaris',
+    'Pseudomonas aeruginosa', 'Pseudomonas fluorescens', 'Pseudomonas gessardii',
+    'Pseudomonas monteilii', 'Pseudomonas putida', 'Pseudomonas stutzeri',
+    'Raoultella ornithinolytica', 'Serratia liquefaciens', 'Serratia marcescens',
+    'Serratia odorifera'
 ]
 
 UNIQUE_PREFIXES_LIST = [
@@ -31,29 +40,20 @@ UNIQUE_PREFIXES_LIST = [
     {
         'prefix': ' MRSA',
         'cultures': ['Staphylococcus aureus'],
-        'resistant_ab': [
-            'Moxifloxacin', 'Erytromycin', 'Clindamycin', 'Ciprofloxacin'
-        ]
+        'resistant_ab': ['Cefoxitin']
     },
     # Extended Spectrum Betalactamas
     # Бета-лактамазы расширенного спектра действия
     {
         'prefix': ' ESBL',
-        'cultures': [
-            'Escherichia coli', 'Klebsiella aerogenes', 'Klebsiella pneumoniae',
-            'Proteus mirabilis', 'Enterobacter cloacae', 'Citrobacter freundii',
-            'Enterobacter asburiae'
-        ],
-        'resistant_ab': [
-            'Ampicillin', 'Amoxicillin/Clavulanic', 'Cefotaxime',
-            'Ceftazidime', 'Cefepime'
-        ]
+        'cultures': CR_ESBLS_CULTURES,
+        'resistant_ab': ['Cefotaxime', 'Ceftazidime', 'Cefepime', 'Ceftriaxone']
     },
     # Carbapenem-resistance Enterobacteriaceae
     # Карбопенем-устойчивые энтеробактерии
     {
-        'prefix': ' CRE',
-        'cultures': ['Klebsiella pneumoniae', 'Klebsiella oxytoca'],
-        'resistant_ab': ['Imipenem', 'Meropenem', 'Cefepime']
+        'prefix': ' CR',
+        'cultures': CR_ESBLS_CULTURES,
+        'resistant_ab': ['Imipenem', 'Meropenem']
     }
 ]
