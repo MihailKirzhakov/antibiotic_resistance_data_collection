fbf4320f68fb164949f3f3901ff556223b63c3f9
Mihail Kirzhakov <kirzhakovma@mail.ru>
2025-05-13 13:29:55 +0300

    'Изменено название логфайла'

--- params/config.py
+++ params/config.py
@@ -11,7 +11,7 @@ from pydantic_settings import BaseSettings
 # Настраиваем лог обработчик
 logger.remove()
 logger.add(
-    sink='log_info.log', level="INFO", rotation='5 MB', mode='a'
+    sink='ADRC_info.log', level="INFO", rotation='5 MB', mode='a'
 )
 
 # -----------------------------------------------------------------
